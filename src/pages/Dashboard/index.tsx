6b6876cdd6d0affe49ee4b7071485e7437402bcb
Leonardo Guth <leoguth55@hotmail.com>
2021-01-23 11:25:05 +0000

    Save repositories on localstorage and Link with the index Repository

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { Title, Form, Repositories, Error } from './styles';
 import logo from '../../assets/logo.svg';
 import { FiChevronRight } from 'react-icons/fi';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
 
 interface Repository {
   full_name: string;
@@ -22,7 +23,23 @@ const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
   // const [inputError, setInputError] = useState('');
   const [inputError, setInputError] = useState<IError>({ status: false });
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem(
+      '@Githubexplorer:repositories'
+    );
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    } else {
+      return [];
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@Githubexplorer:repositories',
+      JSON.stringify(repositories)
+    );
+  }, [repositories]);
 
   async function handleAddRepository(
     event: FormEvent<HTMLFormElement>
@@ -71,7 +88,11 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map((repository) => (
-          <a key={repository.full_name} href="teste">
+          <Link
+            key={repository.full_name}
+            href="teste"
+            to={`repository/${repository.full_name}`}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -81,7 +102,7 @@ const Dashboard: React.FC = () => {
               <p>{repository.description}</p>
             </div>
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
